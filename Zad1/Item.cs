074d62e7a6c47a6b7890241e55624640545e76fb
Jakub Michalski <jakmic100000@gmail.com>
2021-03-12 12:39:39 +0100

    dodane elementy do Plecak i Item
    błąd przy czytaniu Item

--- Zad1/Item.cs
+++ Zad1/Item.cs
@@ -2,7 +2,37 @@
 {
     public class Item
     {
-        public int weight;
-        public int value;
+        private int weight;
+        private int value;
+
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public Item(int value,int weight)
+        {
+            Value = value;
+            Weight = weight;
+        }
     }
 }
--- Zad1/Plecak.cs
+++ Zad1/Plecak.cs
@@ -8,12 +8,32 @@ namespace Zad1
 {
     class Plecak
     {
+        public List<Item> itemList;
         public int valueMax;
         public int weightMax;
 
         private int valueCurrent;
         private int weightCurrent;
+        private int itemsAmount;
 
-        public List<Item> itemList;
+        
+
+        public Plecak(int seed,int itemsAmount)
+        {
+            this.itemsAmount = itemsAmount;
+            RandomNumberGenerator rng = new RandomNumberGenerator(seed);
+            for (int i = 0; i <= itemsAmount; i++)
+            {
+                itemList.Add(new Item(rng.nextInt(1, 29), rng.nextInt(1, 29)));
+            }
+        }
+
+        public void WriteList()
+        {
+            foreach(var x in itemList)
+            {
+                Console.WriteLine(x);
+            }
+        }
     }
 }
