ec57f60ad3d101bc623778831ab22444f499a577
Jakub Michalski <jakmic100000@gmail.com>
2021-03-12 12:23:17 +0100

    Dodanie Plecaka i Item

---
+++ Zad1/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Zad1
+{
+    public class Item
+    {
+        public int weight;
+        public int value;
+    }
+}
---
+++ Zad1/Plecak.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Plecak
+    {
+        public int valueMax;
+        public int weightMax;
+
+        private int valueCurrent;
+        private int weightCurrent;
+
+        public List<Item> itemList;
+    }
+}
